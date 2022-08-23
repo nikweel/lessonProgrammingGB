d3098517f75b9abc0fc2a72e10e692a2110dc9a0
Николай Федотов <nikweel@nikweel.local>
2022-08-23 17:54:42 +0200

    add hometask3 star

---
+++ lession4HomeTask/HomeTask3_star/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+string inputLine;
+int lengthArray;
+int arrayStart;
+int arrayEnd;
+
+System.Random numberSintezator = new Random();
+
+void readData()
+{
+    Console.WriteLine("Введите длину массива и диапазон значение пример 12,44,90");
+    inputLine = Console.ReadLine();
+
+    string[] subs = inputLine.Split(' ',',');
+    lengthArray = int.Parse(subs[0]);
+    arrayStart = int.Parse(subs[1]);
+    arrayEnd = int.Parse(subs[2]);
+
+}
+
+void variantNaiv(){
+    int i = 0;
+    Console.Write("[");
+    while(i < lengthArray - 1){
+        Console.Write(numberSintezator.Next(arrayStart,arrayEnd + 1) + ",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(arrayStart,arrayEnd + 1));
+    Console.Write("]");
+}
+
+readData();
+variantNaiv();
+
+
+
+
+
