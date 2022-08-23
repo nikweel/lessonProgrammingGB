afb17cca576ce2a927b2987347372a4e213a06ef
Николай Федотов <nikweel@nikweel.local>
2022-08-23 17:09:02 +0200

    Задача со звездочкой 1

---
+++ lession4HomeTask/HomeTask1_star/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+string inputLine;
+int inputLineA;
+string inputLineFn;
+int inputLineB;
+double calc = 0;
+
+void readData()
+{
+    Console.WriteLine("Введите вырожение в формате A,действие,B, пример 4,*,6");
+    inputLine = Console.ReadLine();
+
+    inputLineA = int.Parse(inputLine.Substring(0,inputLine.IndexOf(",")));
+    inputLineFn = inputLine.Substring(inputLine.IndexOf(",")+1).Substring(0,inputLine.Substring(inputLine.IndexOf(",")+1).IndexOf(","));
+    inputLineB = int.Parse(inputLine.Substring(inputLine.IndexOf(",")+1).Substring(inputLine.Substring(inputLine.IndexOf(",")+1).IndexOf(",") + 1));
+
+}
+
+void calculation()
+{
+    switch (inputLineFn)
+    {
+        case "+":
+            calc = inputLineA + inputLineB;
+            break;
+        case "-":
+            calc = inputLineA - inputLineB;
+            break;
+        case "*":
+            calc = inputLineA * inputLineB;
+            break;
+        case "/":
+            calc = inputLineA / inputLineB;
+            break;
+        case "pow":
+            calc = Math.Pow(inputLineA,inputLineB);
+            break;
+    }
+}
+
+void printData(){
+    Console.WriteLine(calc);
+}
+
+readData();
+calculation();
+printData();
+
+
+
