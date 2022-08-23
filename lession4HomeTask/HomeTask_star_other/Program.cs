e13037a22b97787aac1d41c08c378fddf5fc9548
Николай Федотов <nikweel@nikweel.local>
2022-08-23 17:36:51 +0200

    add other star home task

---
+++ lession4HomeTask/HomeTask_star_other/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+string inputLine;
+string[] subs;
+System.Random numberSintezator = new Random();
+
+void readData()
+{
+    Console.WriteLine("Введите через запятую имена");
+    inputLine = Console.ReadLine();
+    subs  = inputLine.Split(',');
+    
+}
+
+void randomName()
+{
+    Console.Write(subs[numberSintezator.Next(0,subs.Length)]);
+}
+
+
+readData();
+randomName();
+
+
+
+
