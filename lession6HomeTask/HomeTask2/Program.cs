289cf3f8e98d4610477be253cb53a57fd5205487
Николай Федотов <nikweel@nikweel.local>
2022-08-30 18:09:35 +0200

    add home task 6.2

--- lession6HomeTask/HomeTask2/Program.cs
+++ lession6HomeTask/HomeTask2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+double k1;
+double k2;
+double b1;
+double b2;
+double x;
+double y;
+
+
+//Метод ввода чисел
+void readData()
+{
+    Console.WriteLine("Введите b1");
+    b1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите k1");
+    k1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите b2");
+    b2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите k2");
+    k2 = int.Parse(Console.ReadLine());
+}
+
+//Решение задачи
+string ColculateTask()
+{
+    x = (b2 - b1) / (k2 - k1);
+    y = (k1 * (b2 - b1)) / (k2 - k1) + b1;
+
+    return "("+x+";"+y+")";
+}
+
+//Вывод значения
+void PrintResult(string coord){
+    Console.Write(coord);
+}
+
+readData();
+PrintResult(ColculateTask());
