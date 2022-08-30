e480042b9ab35d708422c019db56f9d0657b0ef9
Николай Федотов <nikweel@nikweel.local>
2022-08-30 16:51:09 +0200

    add home task 6

---
+++ lession6HomeTask/HomeTask1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+string inputLine;
+string[] numbers;
+
+//Метод ввода чисел
+void readData()
+{
+    Console.WriteLine("Введите числа");
+    inputLine = Console.ReadLine();
+    numbers = inputLine.Split(',');
+    Console.WriteLine(numbers);
+}
+
+//Решение задачи
+int ColculateTask(string[] inputArray)
+{
+    int sum = 0;
+    int i = 0;
+    while(i<inputArray.Length){
+
+        if(int.Parse(inputArray[i]) > 0){
+            sum++;
+        }
+        i++;
+    }
+
+    return sum;
+}
+
+
+readData();
+Console.Write(ColculateTask(numbers));
+
