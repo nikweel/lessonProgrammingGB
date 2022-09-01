69c6fec375551d475e72b5116e9d94d5817b9faf
nikweel <nikweel@gmail.com>
2022-09-01 18:32:17 +0200

    add home task 7.2

---
+++ lession7HomeTask/HomeTask2/Program.cs
@@ -0,0 +1,68 @@
+﻿//Метод для заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn){
+    System.Random numbersSyntezator = new System.Random();
+    int i = 0;
+    int j = 0;
+    int[,] outArray = new int[countRow,countColumn];
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i,j] = numbersSyntezator.Next(0,100);
+            j++;
+        }
+
+        i++;
+    }
+    return outArray;
+}
+
+//Метод печати двумерного массива
+void PrintTeoDimArray(int[,] inputArray){
+    int i = 0;
+    int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i,j] + " ");
+            j++;
+        }
+        Console.WriteLine();
+        i++;
+    }
+}
+
+//Метод определения нахождения элемента в массиве
+string SearchElement(int[,] inputArray, int element){
+    int i = 0;
+    int j = 0;
+    string answer = "Элемента ("+element+") нет";
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+
+            if(element == inputArray[i,j]){
+                answer = "Элемент ("+element+") находится на позиции: "+i+" "+j;
+                break;
+            }
+
+            j++;
+        }
+
+        i++;
+    }
+
+    return answer;
+    
+}
+
+int[,] twoDimArray = FillTwoDimArray(5,8);
+PrintTeoDimArray(twoDimArray);
+Console.WriteLine(SearchElement(twoDimArray, 10));
+
+
